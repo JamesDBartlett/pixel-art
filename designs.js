17609772e851766d8ee74ed4ed33a036628c1a54
James D. Bartlett III <37491308+JamesDBartlett@users.noreply.github.com>
2018-03-18 02:04:36 -0500

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,39 +1,52 @@
-// -------- DECLARE VARIABLES & OBJECTS ----------
-// declare grid object
-var g = {
-  dim: [], // grid dimensions
-  dat: [], // grid data
-  c: [], // grid color
+// ------------ VARIABLES & OBJECTS --------------
+// grid object
+var grid = {
+  height: "",
+  width: "",
+  color: "",
+  // data: [[]], to store
 }
-
 // -----------------------------------------------
 
 
-// -------------- DECLARE FUNCTIONS --------------
-// create table with height of g.dim[0] and width of g.dim[1]
+// ----------------- FUNCTIONS -------------------
+// create table with height of grid.height and width of grid.width
 function makeGrid() {
-
+  for(var i = 0; i < grid.height; i++){
+    var row = $("<tr></tr>");
+    for(var j = 0; j < grid.width; j++){
+      var cell = $("<td></td>").attr('id', i + "_" + j);
+      row.append(cell);
+    }
+    $('#pixelCanvas').append(row);
+  }
 }
 
 // -----------------------------------------------
 
 
-// ------- GRID DIMENSIONS EVENT HANDLERS --------
-// when user changes grid height, store value in g.dim[0]
+// ------- GRID ATTRIBUTES EVENT HANDLERS --------
+// when user changes grid height, store value in grid.height
 $('#inputHeight').change(function(event){
-  g.dim[0] = $(this).val();
+  grid.height = Math.round($(this).val()); //rounds off decimals
 });
-// when user changes grid width, store value in g.dim[1]
+// when user changes grid width, store value in grid.width
 $('#inputWidth').change(function(event){
-  g.dim[1] = $(this).val();
+  grid.width = Math.round($(this).val()); //rounds off decimals
+});
+// when user changes grid color, store value in grid.color
+$('#colorPicker').change(function(event){
+  grid.color = $(this).val();
 });
 // -----------------------------------------------
+
 
 
 // -------- FORM SUBMIT EVENT HANDLER ------------
 // when user submits form...
 $('#sizePicker').submit(function(event){
-  event.preventDefault(); // prevent default form action and
+  event.preventDefault(); // prevent default form action
+  $('#pixelCanvas').children().remove();
   makeGrid(); // call the makeGrid function
 });
 // -----------------------------------------------
