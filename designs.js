b9190a2a083b1e2ed092a5c69dfbe83c871ca97b
James D. Bartlett III <37491308+JamesDBartlett@users.noreply.github.com>
2018-03-18 00:05:56 -0500

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,10 +1,39 @@
-// Select color input
-// Select size input
+// -------- DECLARE VARIABLES & OBJECTS ----------
+// declare grid object
+var g = {
+  dim: [], // grid dimensions
+  dat: [], // grid data
+  c: [], // grid color
+}
 
-// When size is submitted by the user, call makeGrid()
+// -----------------------------------------------
 
+
+// -------------- DECLARE FUNCTIONS --------------
+// create table with height of g.dim[0] and width of g.dim[1]
 function makeGrid() {
 
-// Your code goes here!
-
 }
+
+// -----------------------------------------------
+
+
+// ------- GRID DIMENSIONS EVENT HANDLERS --------
+// when user changes grid height, store value in g.dim[0]
+$('#inputHeight').change(function(event){
+  g.dim[0] = $(this).val();
+});
+// when user changes grid width, store value in g.dim[1]
+$('#inputWidth').change(function(event){
+  g.dim[1] = $(this).val();
+});
+// -----------------------------------------------
+
+
+// -------- FORM SUBMIT EVENT HANDLER ------------
+// when user submits form...
+$('#sizePicker').submit(function(event){
+  event.preventDefault(); // prevent default form action and
+  makeGrid(); // call the makeGrid function
+});
+// -----------------------------------------------
