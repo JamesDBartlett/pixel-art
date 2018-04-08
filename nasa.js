1357dc38706cad233a9b8ee3cb33e7aeef37897f
James D. Bartlett III <jimmyb@gmail.com>
2018-04-08 00:17:44 -0500

    update

--- nasa.js
+++ nasa.js
@@ -2,10 +2,8 @@ $(function () {
     var key = 'KlN2nGZbuMg9uOy1AIbmIx7b3oAaClQSe3VJHQNA';
     var $section1 = $('body');
     var nextImage = $('#newbg');
-    var mainContent = $('bgbutton');
 
     function insertNewBackground() {
-        mainContent.addClass('hidden');
 
         function random(min, max) {
             return result = Math.floor(Math.random() * (max - min + 1)) + min; // create random year, day, and month
@@ -16,15 +14,15 @@ $(function () {
         var day = random(1, 28);
 
         var date = year + '-' + month + '-' + day;
-        var url = 'https://api.nasa.gov/planetary/apod?api_key=' + key + '&date=' + date + '&hd=true'; // add random date to url
+        var url = 'https://api.nasa.gov/planetary/apod?api_key=' + key + '&date=' + date; // add random date to url
 
         $.ajax({
             url: url
         }).done(function (response) {
-            var background = response.hdurl;
+            var background = response.url;
             //nextImage.fadeIn(5000);
             $section1.fadeIn(5000).css('background-image', 'url(' + background + ')');
-            imageDate.text('Taken: ' + date);
+            //imageDate.text('Taken: ' + date);
             //mainContent.delay(3000).fadeIn(6000);
         }).fail(function (error) {
             console.log(error);
@@ -34,7 +32,6 @@ $(function () {
     insertNewBackground();
 
     nextImage.on('click', function () {
-        $(this).fadeOut(1000);
         insertNewBackground();
     });
 });
